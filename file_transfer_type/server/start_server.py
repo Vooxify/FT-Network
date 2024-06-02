01384d638e32018f0bc01a54662ea14a966e6cde
name <leopaulchutell@gmail.com>
2024-06-02 21:24:49 +0200

    Test new things

--- file_transfer_type/server/start_server.py
+++ file_transfer_type/server/start_server.py
@@ -7,22 +7,14 @@ context = Context()
 # Global
 
 def handle_client(client_socket):
-    # Afficher une notification lorsqu'un nouveau client se connecte
-    print("[+]  New connection.")
 
     try:
-        # Recevoir les données envoyées par le client
-        message = client_socket.recv(1024).decode()
-
+        message = client_socket.recv().decode()
         while message:
-            # Afficher le message reçu
-            print('Message reçu : ', message)
-
-            # Recevoir les données suivantes envoyées par le client
-            message = client_socket.recv(1024).decode()
+            file_path = message.split("/")[-1]
+            print(file_path)
 
     finally:
-        # Fermer la connexion avec le client
         client_socket.close()
 
 def start_server():
@@ -35,7 +27,6 @@ def start_server():
     sock.bind((context.get("IP"), context.get("PORT")))
     sock.listen()
     while True:
-        print("[+]  Waiting connection.")
         client_socket, client_address = sock.accept()
 
         t = threading.Thread(target=handle_client, args=(client_socket,))
