52e867abc7ef7f59a53591fa4ae76edb443b1ce7
name <leopaulchutell@gmail.com>
2024-06-04 16:28:37 +0200

    Reset the server receive method

--- file_transfer_type/server/start_server.py
+++ file_transfer_type/server/start_server.py
@@ -10,16 +10,3 @@ def start_server():
 
     # FOR DEV REMOVE IF YOU USE !!
     context.set("IP", "localhost")
-
-    sock = context.get("SOCKET")
-    print(context.get("IP"), context.get("PORT"))
-    addr = (context.get("IP"), context.get("PORT"))
-    sock.bind(addr)
-    sock.listen(1)
-
-
-
-    while True:
-        client_socket, client_address = sock.accept()
-        data = client_socket.recv(1024).decode()
-        print(f"Received data : {data}")
